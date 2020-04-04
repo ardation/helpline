963fd322c347bb91b811050d38dd537f5d28ca56
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-04-04 15:01:22 +1300

    update failing spec

--- spec/factories/country/subdivisions.rb
+++ spec/factories/country/subdivisions.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :country_subdivision, class: 'Country::Subdivision' do
-    association :country, factory: :country, code: 'US'
+    country { Country.find_by(code: 'US') || create(:country, code: 'US') }
     after(:build) do |subdivision|
       subdivision.code ||= subdivision.country.iso_3166_country.subdivisions.keys.sample
     end
